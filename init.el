a5320f44733bfd7444278df844b8cc445b44b723
ghodawalaaman <ghodawalaaman@gmail.com>
2025-02-04 22:30:55 +0530

    changes

--- init.el
+++ init.el
@@ -32,15 +32,3 @@
 (global-set-key (kbd "C-c e -") 'emms-volume-lower)
 
 (setq lem-instance-url "https://lemmy.ca")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(0x0 emms-setup emms lem gruvbox-theme)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
