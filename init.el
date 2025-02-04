42d4f40ef6c55379424e7a957130ac659a5beafd
ghodawalaaman <ghodawalaaman@gmail.com>
2025-02-04 22:01:05 +0530

    initial commit

---
+++ init.el
@@ -0,0 +1,33 @@
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/") t)
+
+(menu-bar-mode -1) ;; remove menubar
+(tool-bar-mode -1) ;; remove toolbar
+(setq inhibit-splash-screen t);; remove the splash screen
+
+(set-frame-font "Fira Code-16" nil t);; setting some good fonts
+
+(use-package gruvbox-theme
+  :ensure t
+  :config
+  (load-theme 'gruvbox-dark-medium t))
+(use-package erc)
+(use-package lem)
+(use-package emms)
+
+(setq erc-autojoin-channels-alist
+          '(("Libera.chat" "#emacs" "#bash" "#bsah" "#archlinux" "#gentoo" "#rust")))
+
+(require 'emms-setup)
+(emms-standard) ;; Initialize EMMS with standard settings
+(emms-default-players) ;; Set up default players
+(setq emms-player-mpv-parameters '("--volume=50"))
+(global-set-key (kbd "C-c e +") 'emms-volume-raise)
+(global-set-key (kbd "C-c e -") 'emms-volume-lower)
+
+(setq lem-instance-url "https://lemmy.ca")
