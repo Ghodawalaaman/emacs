ebbceebc1099bc78ae78f1336e56187688239c54
ghodawalaaman <ghodawalaaman@gmail.com>
2025-02-04 22:16:33 +0530

    Added 0x0.st

--- init.el
+++ init.el
@@ -19,6 +19,7 @@
 (use-package erc)
 (use-package lem)
 (use-package emms)
+(use-package 0x0)
 
 (setq erc-autojoin-channels-alist
           '(("Libera.chat" "#emacs" "#bash" "#bsah" "#archlinux" "#gentoo" "#rust")))
@@ -31,3 +32,17 @@
 (global-set-key (kbd "C-c e -") 'emms-volume-lower)
 
 (setq lem-instance-url "https://lemmy.ca")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" default))
+ '(package-selected-packages '(0x0 emms lem rust-mode gruvbox-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
