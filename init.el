18c6119afcbddafb3a5034047e24d3fd6168d86c
ghodawalaaman <ghodawalaaman@gmail.com>
2025-02-11 20:04:25 +0530

    added a channel and removed social media

--- init.el
+++ init.el
@@ -10,17 +10,15 @@
 (tool-bar-mode -1) ;; remove toolbar
 (setq inhibit-splash-screen t);; remove the splash screen
 
-(set-frame-font "Fira Code-14" nil t);; setting some good fonts
+(set-frame-font "FiraCode Nerd Font Mono-14" nil t);; setting some good fonts
 
 (use-package gruvbox-theme
   :ensure t
   :config
   (load-theme 'gruvbox-dark-medium t))
 (use-package erc :ensure t)
-(use-package lem :ensure t)
 (use-package emms :ensure t)
 (use-package 0x0 :ensure t)
-(use-package mastodon :ensure t)
 (use-package expand-region :ensure t)
 (use-package rust-mode :ensure t)
 (use-package elpher :ensure t)
@@ -36,10 +34,7 @@
 (global-set-key (kbd "C-=") 'er/expand-region)
 
 (setq erc-autojoin-channels-alist
-      '(("Libera.chat" "#emacs" "#bash" "#bsah" "#archlinux" "#gentoo" "#rust" "#gemini" "#git")))
-
-(setq mastodon-instance-url "https://social.linux.pizza"
-      mastodon-active-user "unknown231")
+      '(("Libera.chat" "#emacs" "#bash" "#bsah" "#archlinux" "#gentoo" "#rust" "#gemini" "#git" "#lisp")))
 
 (require 'emms-setup)
 (emms-standard) ;; Initialize EMMS with standard settings
